763130e5108abbd9fb08e77fbe3bed612251f52a
mrevening <dominicwieczo@gmail.com>
2024-11-10 21:53:27 +0100

    f

--- LifeTest/CellTests.cs
+++ LifeTest/CellTests.cs
@@ -8,25 +8,32 @@ namespace LifeTest
         public void Cell_dies_when_one_living_neighbour_cell()
         {
             var uat = new Cell(true, 1);
-            uat.
+            var endState = uat.CalculateState();
+            Assert.Equal(State.Dies, endState);
         }
 
         [Fact]
         public void Cell_dies_when_four_or_more_living_neighbour_cells()
         {
-
+            var uat = new Cell(true, 4);
+            var endState = uat.CalculateState();
+            Assert.Equal(State.Dies, endState);
         }
 
         [Fact]
         public void Cell_survies_when_two_or_three_neighbour_cells()
         {
-
+            var uat = new Cell(true, 2);
+            var endState = uat.CalculateState();
+            Assert.Equal(State.Lives, endState);
         }
 
         [Fact]
         public void Cell_is_born_when_three_neighbour_cells()
         {
-
+            var uat = new Cell(false, 3);
+            var endState = uat.CalculateState();
+            Assert.Equal(State.Lives, endState);
         }
     }
 }
